1fbd0f8d49efadb9968023b1d437d315c68d6fc9
thien.vuong <thien.vuong@nois.vn>
2023-10-16 23:00:27 +0700

    Setup + Create Button Element

---
+++ src/app/element/button/button.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent {
+  @Input() icon: any = "Icon";
+  @Input() text: string = "Button";
+}
