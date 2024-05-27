4906ad333d06e19760b305ddd1b6598172c02299
thien.vuong <thien.vuong@nois.vn>
2024-05-28 05:33:13 +0700

    Footer Section

--- src/app/section/footer-section/footer-section.component.ts
+++ src/app/section/footer-section/footer-section.component.ts
@@ -8,24 +8,42 @@ import { Component } from '@angular/core';
 export class FooterSectionComponent {
   socials = [
     {
+      title: "Figma",
       link: 'https://www.figma.com/@vuonghuuthien',
       icon: '/assets/socials/figma.svg'
     },
     {
+      title: "LinkedIn",
       link: 'https://www.linkedin.com/in/vuong-huu-thien/',
       icon: '/assets/socials/linkedin.svg'
     },
     {
+      title: "Behance",
       link: 'https://www.behance.net/vuong_huu_thien',
       icon: '/assets/socials/behance.svg'
     },
     {
+      title: "Dribbble",
       link: 'https://dribbble.com/vuong_huu_thien',
       icon: '/assets/socials/dribbble.svg'
     },
     {
+      title: "X",
       link: 'https://x.com/vuong_huu_thien',
       icon: '/assets/socials/x.svg'
     }
   ];
+
+  email = "vuonghuuthientk@gmail.com";
+  phone = "(+84)905045730";
+
+  copyText(text: string): void {
+    const textArea = document.createElement('textarea');
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+    alert('Đã sao chép vào clipboard!');
+  }
 }
