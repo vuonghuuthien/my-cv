08082c0f2d2e2f3777c518b97687db7f20a3221d
thien.vuong <thien.vuong@nois.vn>
2024-05-30 14:19:07 +0700

    Responsive

--- src/app/page/my-cv/my-cv.component.ts
+++ src/app/page/my-cv/my-cv.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-my-cv',
   templateUrl: './my-cv.component.html',
-  styleUrls: ['./my-cv.component.scss']
+  styleUrls: ['./my-cv.component.scss'],
 })
 export class MyCVComponent {
+  imgSrc = '/assets/cv/MyCV.png';
+  pdfSrc = '/assets/cv/MyCV.pdf';
 
+  downloadCV() {
+    const link = document.createElement('a');
+    link.href = this.pdfSrc;
+    link.download = 'MyCV.pdf';
+    link.click();
+  }
 }
--- src/app/section/left-bar-section/left-bar-section.component.ts
+++ src/app/section/left-bar-section/left-bar-section.component.ts
@@ -3,18 +3,42 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-left-bar-section',
   templateUrl: './left-bar-section.component.html',
-  styleUrls: ['./left-bar-section.component.scss']
+  styleUrls: ['./left-bar-section.component.scss'],
 })
 export class LeftBarSectionComponent {
-  socials = [
-    { label: 'fb' },
-    { label: 'be' },
-    { label: 'in' },
-  ];
+  socials = [{ label: 'fb' }, { label: 'be' }, { label: 'in' }];
 
   roles = [
     { label: 'designer' },
     { label: 'developer' },
     { label: 'animator' },
+  ];
+
+  socials_icon = [
+    {
+      title: 'LinkedIn',
+      link: 'https://www.linkedin.com/in/vuong-huu-thien/',
+      icon: '/assets/socials/LinkedIn2.svg',
+    },
+    {
+      title: 'Behance',
+      link: 'https://www.behance.net/vuong_huu_thien',
+      icon: '/assets/socials/Behance2.svg',
+    },
+    {
+      title: 'Figma',
+      link: 'https://www.figma.com/@vuonghuuthien',
+      icon: '/assets/socials/Figma2.svg',
+    },
+    {
+      title: 'Dribbble',
+      link: 'https://dribbble.com/vuong_huu_thien',
+      icon: '/assets/socials/Dribbble2.svg',
+    },
+    {
+      title: 'X',
+      link: 'https://x.com/vuong_huu_thien',
+      icon: '/assets/socials/X2.svg',
+    },
   ];
 }
