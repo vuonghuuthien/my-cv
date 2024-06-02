fcfd99208fa3da49b1c4c4506d53ec4bee0a9d29
thien.vuong <thien.vuong@nois.vn>
2024-06-02 16:24:16 +0700

    Adđ Google Analysis + Compress Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Router, Event, NavigationEnd } from '@angular/router';  // Import thêm Event
+import { filter } from 'rxjs/operators';  // Import thêm filter
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +9,9 @@ import { ElementModule } from './element/element.module';
 import { SectionModule } from './section/section.module';
 import { DirectiveModule } from './directive/directive.module';
 import { PageModule } from './page/page.module';
+
+// Khai báo gtag như một hàm toàn cục
+declare let gtag: Function;
 
 @NgModule({
   declarations: [
@@ -23,4 +28,14 @@ import { PageModule } from './page/page.module';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(router: Router) {
+    router.events.pipe(
+      filter((event: Event): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      gtag('config', 'G-SM7RFN6T2V', {
+        'page_path': event.urlAfterRedirects
+      });
+    });
+  }
+}
